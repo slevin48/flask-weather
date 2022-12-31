6d0c4f34af98142697d8a272a75a27884b7826fa
Slevin48 <yann.debray@gadz.org>
2022-12-31 18:47:39 +0100

    Get input from form

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import weather
 
 app = Flask(__name__)
 appid='b1b15e88fa797225412429c1c50c122a1'
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route("/", methods=['POST'])
-def get_weather():
-    # get weather message
-    json_data = weather.get_current_weather('London','uk',appid,api='samples')
-    data = weather.parse_current_json(json_data)
-    return render_template('index.html', weather_message=data['temp']-273.15)
+    if request.method == 'POST':
+        # get the value from the form
+        city = request.form['city']
+        country = request.form['country']
+        json_data = weather.get_current_weather(city,country,appid,api='samples')
+        data = weather.parse_current_json(json_data)
+        return render_template('index.html', results=data['temp']-273.15)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
